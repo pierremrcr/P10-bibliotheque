314ac74794d81155ca998c4f2d69f01c3ae24a6c
pierremrcr <pierremercier4@gmail.com>
2020-11-03 14:21:42 +0100

    EmpruntServiceImplTest updated

--- bibliotheque/src/test/java/com/bibliotheque/service/EmpruntServiceImplTest.java
+++ bibliotheque/src/test/java/com/bibliotheque/service/EmpruntServiceImplTest.java
@@ -59,6 +59,11 @@ public class EmpruntServiceImplTest {
 
     }
 
+    @Test
+    public void getAllEmpruntsTest(){
+
+    }
+
     public static Date addDays(Date date, int days) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
